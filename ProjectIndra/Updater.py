02af2ab1c77f18b7ddf442ca129c80695009d54c
Hartsaxena <Hartsaxena@users.noreply.github.com>
2020-04-26 02:29:07 -0500

    Update 0.11

--- ProjectIndra/Updater.py
+++ ProjectIndra/Updater.py
@@ -1,41 +1,33 @@
-# This is an Updater program that executes when you decide to Update the game
-# Only execute and/or edit this program sparingly, or else you could seriously break Something
-# This took me so long to debug...
-
-
 import git
 import time
 import shutil
 import os
-from os import getcwd
 import os.path
-import requests
 from pathlib import Path
-yesno = input("Are you sure? doing this will update the game to the current\nversion (In the github page). This will also reset the game.\nIn order to save progress, make a copy of the 'saves' file\nthat can be found in the Saves folder in the ProjectIndra folder.\n[y/n]\n")
+yesno = input("Are you sure? doing this will update the game to the current\nversion (In the github page). This will also reset the game.\nIn order to save progress, make a copy of the 'saves' file\nthat can be found in the Saves folder in the ProjectIndra folder.\n[y/n] ")
 if yesno.lower() == 'yes' or yesno.lower() == 'y' or yesno.lower() == 'yep':
     print("Ok!")
     time.sleep(1)
     print("Warning! Don't terminate this program while this is being run.\nDoing so can and most likely will\ncause serious problems.")
     print("Checking if a Download folder currently exists...")
-    if os.path.exists(Path.home()/"Documents/ProjectIndra"):
-        print ("Deleting Download Folder...")
-        shutil.rmtree(Path.home()/"Documents/ProjectIndra")
-    print("Removing old files...")
-    if os.path.exists(Path.home()/"Documents/Indra.py")==True:
-        try:
-            os.remove(Path.home()/"Documents/Indra.py")
-        except PermissionError:
-            shutil.rmtree(Path.home()/"Documents/Indra.py")
+    if os.path.exists(Path.home()/"Downloads/Project-Indra"):
+        shutil.rmtree(Path.home()/"Downloads/Project-Indra")
     print("Downloading most recent version of Project Indra...")
-    git.Git(Path.home()/"Documents").clone("https://github.com/Hartsaxena/ProjectIndra.git")
-    url = 'https://raw.githubusercontent.com/Hartsaxena/Indra.py/master/Indra.py'
-    r = requests.get(url, allow_redirects=True)
-    open(Path.home()/"Documents/Indra.py", 'wb').write(r.content)
-    # git.Git(Path.home()/"Documents").clone("https://github.com/Hartsaxena/Indra.py.git")
-    print("Removing Useless files...")
-    if os.path.exists(Path.home()/"Documents/LICENSE") == True:
-        os.remove(Path.home()/"Documents/LICENSE")
-    if os.path.exists(Path.home()/"Documents/README.md") == True:
-        os.remove(Path.home()/"Documents/README.md")
-
+    git.Git(Path.home()/"Downloads").clone("https://github.com/Hartsaxena/Project-Indra.git")
+    print("Removing old files...")
+    indrafolder = Path(Path.home()/"Documents/ProjectIndra")
+    shutil.rmtree(indrafolder/"Executes")
+    shutil.rmtree(indrafolder/"Images")
+    shutil.rmtree(indrafolder/"Misc")
+    shutil.rmtree(indrafolder/"Vitals")
+    os.remove(indrafolder/"DownloadGuide.txt")
+    os.remove(indrafolder/"Sunlight.txt")
+    os.remove(Path.home()/"Documents/Indra.py")
+    print("Moving files...")
+    original = Path(Path.home()/"Downloads/Project-Indra/ProjectIndra/")
+    target = Path(Path.home()/"Documents/ProjectIndra")
+    original1 = Path(Path.home()/"Downloads/Project-Indra/Indra.py")
+    target1 = Path(Path.home()/"Documents")
+    shutil.move(original.absolute(), target.absolute())
+    shutil.move(original1.absolute(), target1.absolute())
     print ("Done!")
